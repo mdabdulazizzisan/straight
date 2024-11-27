7ed293bf088ca1eb95233dcb0a41ad0df997433d
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2024-11-27 08:12:04 +0600

    including functions from the functions folder.

---
+++ main.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# including functions
+source functions/detect_package_manager.sh
+source functions/list_software.sh
+source functions/install_software.sh
+source data/software_list.sh
