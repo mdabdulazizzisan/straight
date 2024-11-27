db80925cc26ec23c451ee014b2009150d040ca14
Md Abdul Aziz Zisan <77972913+mdabdulazizzisan@users.noreply.github.com>
2024-11-27 08:57:21 +0600

    handling user input

--- main.sh
+++ main.sh
@@ -5,3 +5,7 @@ source functions/detect_package_manager.sh
 source functions/list_software.sh
 source functions/install_software.sh
 source data/software_list.sh
+
+# user input
+echo "Enter space-separated short names of softwares you want to install:"
+read -r selected_software
